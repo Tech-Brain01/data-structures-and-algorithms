2f08a5fd5e735990374b25f926425789e1fe07c9
Tech-Brain01 <tomar.amrendera@outlook.com>
2025-09-04 00:05:09 +0530

    add

--- ARRAY/MaxSumSubArray.java
+++ ARRAY/MaxSumSubArray.java
@@ -85,3 +85,32 @@
 //     SumSubArray(numbers);
 //   }
 //  }
+
+
+                                                //  KADANES ALGO
+
+                                                                                     // T.C = o(n)
+
+package ARRAY;
+
+public class MaxSumSubArray {
+
+    public static void Kadanes(int numbers[]) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+
+        for (int i = 0 ; i < numbers.length; i++) {
+             cs = cs + numbers[i];
+             if(cs < 0){
+                cs = 0;
+             }
+             ms = Math.max(ms, cs);
+        }
+        System.out.print("max sum is : " + ms);
+    }
+
+  public static void main(String args[]) {
+    int numbers[] = {-2, -3, 4, -2, -1 , 1, 5 , -3};
+    Kadanes(numbers);
+  }
+}
